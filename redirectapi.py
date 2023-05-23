e166c815ad81f7dd9d9574529391fbcc8807da17
Anisree.Sreekumar <anisree.sreekumar@ust.com>
2023-05-23 10:35:22 +0530

    updates in function

--- redirectapi.py
+++ redirectapi.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 
 @app.route("/ask", methods=["POST"])
 def ask():
     question = request.form["question"]
     answer = ask_chatgpt(question)
     return render_template("answer.html", question=question, answer=answer)
+
+if __name__ =="__main__":
+    app.run(port="5000")
