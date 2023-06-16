096d937cd1ff0100aabbec43d44f455230fd21d4
Anisree G S <anisreegs.2@gmail.com>
2023-06-16 19:45:21 +0530

    added jsonify for response

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request,jsonify
+import openai
+openai.api_key = "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"
+
+
+app = Flask(__name__)
+
+@app.route("/ask", methods=['POST'])
+def ask():
+    question = request.json['question']
+    print("inside ask - question",question)
+    print("inside ask",request)
+    response = openai.Completion.create(
+        engine="davinci",
+        # prompt=f"Q: {question}\nA:",
+        prompt = f"A:",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+    # answer = response
+    answer = response.choices[0].text.strip()
+    returnedAnswer = jsonify(answer)
+    print("answer : " ,response)
+    return returnedAnswer
+    # question = request.body["question"]
+    # answer = ask_chatgpt(question)
+    # return render_template("answer.html", question=question, answer=answer)
+    #return "true"
+
+if __name__ =="__main__":
+    app.run(debug=True)
